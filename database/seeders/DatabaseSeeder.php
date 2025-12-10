dd650536b5bd8db7c553f45434378b75faff73d9
MDashik-develop <mdashik.design.laptop@gmail.com>
2025-12-11 03:17:24 +0600

    change something user

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,9 +2,9 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Modules\User\Models\User;
 
 class DatabaseSeeder extends Seeder
 {
@@ -19,7 +19,8 @@ class DatabaseSeeder extends Seeder
 
         User::factory()->create([
             'name' => 'Test User',
-            'email' => 'test@example.com',
+            'email' => 'admin@gmail.com',
+            'password' => bcrypt('123456'),
         ]);
     }
 }
